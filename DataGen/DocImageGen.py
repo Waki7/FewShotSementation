642d46680edb69fb0235b139d3f66d18cec870ee
Ceyer <c.waki7@gmail.com>
2019-04-25 19:28:06 -0400

    start generator class, and generator for numpy array of lines and whatnot

--- DataGen/DocImageGen.py
+++ DataGen/DocImageGen.py
@@ -1,8 +1,23 @@
+import numpy as np
+
+class DocGenerator():
+    def __initd__(self):
+        self.seed = 5
+        np.random.seed(5)
+        self.horizontalLines = self.createHorizLines()
+        self.verticalLines = self.createVertLines()
+
+    def createHorizLines(self):
+        line =
+        pass
+
+    def createVertLines(self):
+
+        pass
+
 
 def main():
     pass
-
-
 
 if __name__ == '__main__':
     main()
