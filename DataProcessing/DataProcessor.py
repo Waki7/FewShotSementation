c2f231eaed229b88d2dce0b460976dcfd830a28c
Waki7 <c.waki7@gmail.com>
2019-06-19 02:39:49 -0700

    k finished data processing, processed path and processed name would be ideal to be together... also try to see if there is a way to compress stored array size

--- DataProcessing/DataProcessor.py
+++ DataProcessing/DataProcessor.py
@@ -2,6 +2,7 @@ import scipy.io
 import os
 from os.path import join, isfile
 import numpy as np
+import pickle
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 
@@ -10,51 +11,94 @@ class DataSet():
     def __init__(self):
         self.fileExt = None
 
-    def fileReader(self, file):
+    def readFile(self, file):
         raise NotImplementedError
 
-    def process(self, directoryPath):  # should just get all files in directory as ndarray
+    def pad(self, image: np.ndarray, maxShape):
+        pad_width = [(0, maxShape[i] - image.shape[i]) for i in range(len(maxShape))]
+        return np.pad(image,
+                      pad_width=pad_width, mode='constant')
+
+    def saveData(self, path, filename, array):
+        if not os.path.exists(path):
+            os.makedirs(path)
+        # array = np.ndarray(array, dtype=np.float16)
+        with open(path + filename, 'wb') as f: pickle.dump(array, f)
+
+    def loadArray(self, path, filename):
+        with open(path + filename, 'rb') as f: return pickle.load(f)
+
+    def processImages(self, directoryPath, pad = True):
         data = []
+        maxShape = None
         for filename in os.listdir(directoryPath):
             if filename.endswith(self.fileExt):
                 samplePath = join(directoryPath, filename)
-                data.append(self.fileReader(samplePath))
-        return np.ndarray(data)
+                image = self.readFile(samplePath)
+                if maxShape is None:
+                    maxShape = [0]*len(image.shape)
+                for i in range(len(image.shape)):
+                    maxShape[i] = max(image.shape[i], maxShape[i])
+                data.append(self.readFile(samplePath))
+        if pad:
+            data = [self.pad(im, maxShape = maxShape) for im in data]
+        return np.stack(data)
+
 
 class BSRLabels(DataSet):
     def __init__(self):
         super(BSRLabels, self).__init__()
         self.fileExt = '.mat'
         self.rootPath = '..\\Data\\BSR\\BSDS500\\data\\groundTruth\\'
+        self.processedDataPath = self.rootPath + 'ProcessedData\\'
+        self.processedDataName = 'processedLabels.pkl'
         self.paths = [self.rootPath + i for i in ['train', 'test', 'val']]
         self.matKey = 'groundTruth'
         self.segmentationIndex = 0
         self.boundaryIndex = 1
 
-    def fileReader(self, file):
+    def readFile(self, file):
         mat = scipy.io.loadmat(file)
         mat_data = np.squeeze(mat[self.matKey][0, 0]).item(0)
         datum = mat_data[self.segmentationIndex] #segementation ground truth, mat_data[1] is the boundary boxes
         return datum
 
     def getData(self):
+        data = []
+        if isfile(self.processedDataPath + self.processedDataName):
+            data = self.loadArray(self.processedDataPath, self.processedDataName)
+            print('...loaded array of shape ' + str(data.shape))
+            return data
         for set in self.paths:
-            self.process(set)
+            data.append(self.processImages(set, pad=True))
+        data = np.vstack(data)
+        self.saveData(self.processedDataPath, self.processedDataName, data)
+        return data
 
 class BSRImages(DataSet):
     def __init__(self):
         super(BSRImages, self).__init__()
         self.fileExt = '.jpg'
         self.rootPath = '..\\Data\\BSR\\BSDS500\\data\\images\\'
+        self.processedDataPath = self.rootPath + 'ProcessedData\\'
+        self.processedDataName = 'processedImages.pkl'
         self.paths = [self.rootPath + i for i in ['train', 'test', 'val']]
 
-    def fileReader(self, file):
+    def readFile(self, file):
         datum = scipy.misc.imread(file)
         return datum
 
     def getData(self):
+        data = []
+        if isfile(self.processedDataPath + self.processedDataName):
+            data = self.loadArray(self.processedDataPath, self.processedDataName)
+            print('...loaded array of shape ' + str(data.shape))
+            return data
         for set in self.paths:
-            self.process(set)
+            data.append(self.processImages(set, pad=True))
+        data = np.vstack(data)
+        self.saveData(self.processedDataPath, self.processedDataName, data)
+        return data
 
 
 def processBSR():
@@ -66,8 +110,6 @@ def processBSR():
 
 def main():
     x, y = processBSR()
-    print(x.shape)
-    print(y.shape)
 
 
 if __name__=='__main__':
