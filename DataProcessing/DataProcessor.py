loss is nan, fixed label issues for network output, and datatype issues, change to softmax over class dimension, probably need to switch to coco for higher data to class ratio

DataProcessing/DataProcessor.py
@@ -25,7 +25,6 @@ class DataSet():
         if compress:
             if np.issubdtype(array.dtype, np.integer):
                 array = array.astype(np.int8)
-                print(array.dtype)
             if np.issubdtype(array.dtype, np.floating):
                 array = array.astype(np.float16)
         with open(path + filename, 'wb') as f: pickle.dump(array, f)
@@ -66,6 +65,9 @@ class BSRLabels(DataSet):
         mat = scipy.io.loadmat(file)
         mat_data = np.squeeze(mat[self.matKey][0, 0]).item(0)
         datum = mat_data[self.segmentationIndex] #segementation ground truth, mat_data[1] is the boundary boxes
+        # datum1 = mat_data[1]
+        # plt.imshow(datum)
+        # plt.show()
         return datum
 
     def getData(self):
@@ -106,15 +108,15 @@ class BSRImages(DataSet):
         return data
 
 
-def processBSR(dtype = np.uint8):
+def processBSR(x_dtype = np.float16, y_dtype = np.float16):
     labels = BSRLabels()
     images = BSRImages()
     x = images.getData()
     y = labels.getData()
-    if x.dtype != dtype:
-        x = x.astype(dtype)
-    if y.dtype != dtype:
-        y = x.astype(dtype)
+    if x.dtype != x_dtype:
+        x = x.astype(x_dtype)
+    if y.dtype != y_dtype:
+        y = y.astype(y_dtype)
     return x, y
 
 def main():

Model/Segmenter.py
@@ -4,16 +4,14 @@ import torch.nn.functional as F
 import DataProcessing.DataProcessor as data
 import numpy as np
 
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-else:
-    device = torch.device('cpu')
-type = torch.float
+device = torch.device('cuda')
+# device = torch.device('cpu') #todo: can't go between cpu and gpu seamlessly, implementation errors: RuntimeError: _thnn_conv2d_forward is not implemented for type torch.HalfTensor
+type = torch.float16
 args = {'device': device, 'dtype': type}
 
 
 class Segmenter(nn.Module):
-    def __init__(self, inputShape, classes = 1):
+    def __init__(self, inputShape, classes=1):
         super(Segmenter, self).__init__()
 
         numHU = 64
@@ -26,46 +24,53 @@ class Segmenter(nn.Module):
         kernel_sizeP_1 = 3
         out_channels_1 = 18
 
-        self.conv1 = torch.nn.Conv2d(in_channels=channels, out_channels = out_channels_1, kernel_size=kernel_sizeC_1,
-                                     stride=strideC_1, padding=kernel_sizeC_1//2)
+        self.conv1 = torch.nn.Conv2d(in_channels=channels, out_channels=out_channels_1, kernel_size=kernel_sizeC_1,
+                                     stride=strideC_1, padding=kernel_sizeC_1 // 2)
         self.pool1 = torch.nn.MaxPool2d(kernel_size=kernel_sizeP_1,
-                                        stride=strideP_1, padding=kernel_sizeP_1//2)
+                                        stride=strideP_1, padding=kernel_sizeP_1 // 2)
 
         strideC_2 = 1
         kernel_sizeC_2 = 7
 
-        self.conv2 = torch.nn.Conv2d(in_channels=out_channels_1, out_channels = 1, kernel_size=kernel_sizeC_2, #out channels 1 cause labels
-                                     stride=strideC_2, padding=kernel_sizeC_2//2)
+        self.conv2 = torch.nn.Conv2d(in_channels=out_channels_1, out_channels=classes, kernel_size=kernel_sizeC_2,
+                                     # out channels 1 cause labels
+                                     stride=strideC_2, padding=kernel_sizeC_2 // 2)
 
     def forward(self, x):
         # Computes the activation of the first convolution, size will be size of input + padding - kernel size//2
-        c1 = F.relu(self.conv1(x))
+        # get size of memory allocation in bytes : tensorname.element_size() * tensorname.nelement()
+        c1 = torch.sigmoid(self.conv1(x))
         p1 = self.pool1(c1)
-        c2 = F.sigmoid(self.conv2(p1)) # probabilistic interpretation for last layer classification, we could add channels if multiple labels for each pixel
-        return c2
+        c2 = self.conv2(
+            p1)
+        out = F.softmax(c2, dim=1)  # probabilistic interpretation for last layer classification, we could add channels if multiple labels for each pixel
+        # class0 = out.data[0,:,0,0]
+        # print(class0.shape)
+        # print(torch.sum(class0))
+        return out
 
     def getData(self):
         pass
 
+
 def ValidateSegmenter():
     epochs = 10
-    batch_size = 100
+    batch_size = 2
 
-
-    x, y = data.processBSR()
-    x = np.transpose(x, (0,3,1,2))
+    x, y = data.processBSR(x_dtype = np.float16, y_dtype = np.float16)
+    x = np.transpose(x, (0, 3, 1, 2))
+    classes = int(np.max(y))
     n_train = x.shape[0]
-    classes = np.prod(y.shape[1:])
-    model = Segmenter(x.shape).to(device)
+    model = Segmenter(x.shape, classes).to(**args)
     opt = torch.optim.SGD(model.parameters(), lr=.1)
     criterion = torch.nn.CrossEntropyLoss()
     shuffled_indexes = torch.randperm(n_train)
 
     for e in range(epochs):
         for i in range(0, n_train, batch_size):
-            indexes = shuffled_indexes[i:i+batch_size]
-            x_batch = torch.tensor(x[indexes], **args)
-            y_batch = torch.tensor(y[indexes], **args)
+            indexes = shuffled_indexes[i:i + batch_size]
+            x_batch = torch.tensor(x[indexes]).to(**args)
+            y_batch = torch.tensor(y[indexes]).to(device).long() #todo: torch CrossEntropyLoss requires long... too much memory usage, might have to implement myself...
 
             y_out = model.forward(x_batch)
             loss = criterion(input=y_out, target=y_batch)
@@ -73,6 +78,7 @@ def ValidateSegmenter():
             loss.backward(retain_graph=False)
             opt.step()
             opt.zero_grad()
+
 
 def main():
     ValidateSegmenter()