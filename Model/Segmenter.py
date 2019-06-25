1707ef975506e1cc5a89fb5b7aa8b946f0d474d9
Waki7 <c.waki7@gmail.com>
2019-06-24 22:16:12 -0700

    big data pickle.... probably gonna switch to coco dataset

--- Model/Segmenter.py
+++ Model/Segmenter.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import DataProcessing.DataProcessor as data
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -55,33 +56,50 @@ class Segmenter(nn.Module):
 
 
 def ValidateSegmenter():
-    epochs = 10
+    epochs = 100
     batch_size = 4
 
     x, y = data.processBSR(x_dtype = np.float16, y_dtype = np.float16)
+
     assert not np.any(np.isnan(x))
     assert not np.any(np.isnan(y))
     x = np.transpose(x, (0, 3, 1, 2))
     classes = int(np.max(y)+1)
-    n_train = x.shape[0]
+    print(classes)
+    unique, counts = np.unique(y, return_counts=True)
+    print(counts)
+    print(unique)
+    print(sum(counts))
+    print(481*481*500)
+    print(exit(9))
+    n_train = 5#x.shape[0]
     model = Segmenter(x.shape, classes).to(**args)
     opt = torch.optim.Adam(model.parameters(), lr=.00005)
     criterion = torch.nn.CrossEntropyLoss()
     shuffled_indexes = torch.randperm(n_train)
-    # x, y = torch.tensor(x).to(**args), torch.tensor(y).to(device).long()
+    x, y = torch.tensor(x).to(**args), torch.tensor(y).to(device).long()
 
     for e in range(epochs):
         for i in range(0, n_train, batch_size):
             indexes = shuffled_indexes[i:i + batch_size]
             x_batch, y_batch = x[indexes], y[indexes]
-            x_batch, y_batch = torch.tensor(x_batch).to(**args), torch.tensor(y_batch).to(device).long()
+            # x_batch, y_batch = torch.tensor(x_batch).to(**args), torch.tensor(y_batch).to(device).long()
             y_out = model.forward(x_batch)
             loss = criterion.forward(input=y_out, target=y_batch)
             print(loss.data)
             loss.backward(retain_graph=False)
             opt.step()
             opt.zero_grad()
-
+    #arbitrarily look at the first 10 images and see their output
+    for i in range(0, 10):
+        _, ax = plt.subplots(1, 2)
+        prediction = model.forward(x[i:i+1])
+        prediction = prediction.detach().cpu()
+        prediction = np.argmax(prediction, axis=1)
+        prediction = prediction[0]
+        ax[0].imshow(prediction)
+        ax[1].imshow(y[i:i+1].cpu()[0])
+        plt.show()
 
 def main():
     ValidateSegmenter()
