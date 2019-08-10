a86b5c8e1aa128a3e7f9a685dd63615cb630d44e
Ceyer <c.waki7@gmail.com>
2019-08-10 14:58:55 -0400

    we need to totally fix how we control experiments, but added a little more to it for now

--- Model/Config.py
+++ Model/Config.py
@@ -10,7 +10,6 @@ class DataSetNames(Enum):
    BSR = 'BSR'
    VOC = 'VOC'
 
-dataset_name = DataSetNames.VOC
 
 #pytorch
 #################################################################################################################
@@ -23,12 +22,15 @@ torch.cuda.set_device(0)
 
 
 
+model_size = 32
+lr = .075
+dataset_name = DataSetNames.VOC
+experiment = 'FullSeg'
+
 load_model = False
 downsample_ratio = 4
 batch_size = 15 #roughly 45 for 64 model_size and half as u keep doubling
-model_size = 32
 epochs = 1000
-lr = .075
 np.random.seed(24)
 random.seed(24)
 torch.manual_seed(24)
@@ -38,15 +40,15 @@ torch.backends.cudnn.benchmark=False
 
 #Paths
 #################################################################################################################
-experiment_path = '..\\ExperimentResults\\' + dataset_name.value + '\\FullSeg\\ClassWeights\\' + str(model_size) + '\\'
+experiment_path = '..\\ExperimentResults\\' + dataset_name.value + '\\'+experiment+'\\ClassWeights\\' + str(model_size) + '\\'
 processed_data_path = '..\\Data\\ProcessedData\\'
-stored_model_path = join(experiment_path, 'Full'+dataset_name.value+'Segmenter'+str(lr))
+stored_model_path = join(experiment_path, 'model_'+str(lr))
 graph_file_path = join(experiment_path, 'heatmap_confusion_matrix' + str(lr))
 #################################################################################################################
 
 
 if not os.path.exists(experiment_path):
     os.makedirs(experiment_path)
-console_file_name = 'console' + str(lr) + '.txt'
+console_file_name = 'console_' + str(lr) + '.txt'
 console_file_path = join(experiment_path, console_file_name)
 prnt = {'file': open(console_file_path, 'w'), 'flush': True} if not load_model else {}
